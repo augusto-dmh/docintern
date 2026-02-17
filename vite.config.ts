9b2e3573dc3644d66ad065ca99f213a50b0f595d
augusto-dmh <augustodemelohenriques@gmail.com>
2026-02-17 10:45:32 -0300

    feat: update env and vite config for Docker environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
